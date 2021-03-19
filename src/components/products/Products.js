bb5632cf22a1741a4aa61d2c49c8919fcd7ddf54
Sajith Sadanandan <sajith.developer@gmail.com>
2021-03-19 09:56:22 +0530

    swipe action added
    swipe action added

--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -1,19 +1,37 @@
 import React, { useState } from "react";
 import Product from "./Product";
+import Swipe from "react-easy-swipe";
 
 const Products = ({ products }) => {
-  const [width, setWdith] = useState(window.innerWidth);
+  const [width, setWidth] = useState(window.innerWidth);
+  const [xPos, setXPos] = useState(0);
+
   const [containerWidth, setContainerWidth] = useState(
     `${width * products.length}px`
   );
+
+  const onSwipeLeft = (event) => {
+    const newXPos = xPos - width;
+    setXPos(newXPos);
+  };
+  const onSwipeRight = (event) => {
+    const newXPos = xPos + width;
+    setXPos(newXPos);
+  };
+
   return (
-    <div className="listWrapper">
-      <div className="productList" style={{ width: containerWidth }}>
-        {products.map((shoe) => (
-          <Product product={shoe} key={shoe.id} width={width} />
-        ))}
+    <Swipe onSwipeLeft={onSwipeLeft} onSwipeRight={onSwipeRight}>
+      <div className="listWrapper">
+        <div
+          className="productList"
+          style={{ width: containerWidth, transform: `translateX(${xPos}px)` }}
+        >
+          {products.map((shoe) => (
+            <Product product={shoe} key={shoe.id} width={width} />
+          ))}
+        </div>
       </div>
-    </div>
+    </Swipe>
   );
 };
 
